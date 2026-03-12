2eacc17e594130589783e247fb19edd9e6cfc2bf
tuanworlddev <nanhtuan.it@gmail.com>
2026-03-12 13:36:44 +0700

    Init project

---
+++ src/main/java/com/tuandev/strawberrybackend/StrawberryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuandev.strawberrybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawberryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawberryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuandev/strawberrybackend/StrawberryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandev.strawberrybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawberryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
